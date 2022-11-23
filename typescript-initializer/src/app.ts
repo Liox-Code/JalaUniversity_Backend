99f6598288c40821ad52ab02ac7d0e1ef08cd740
Angel Leonardo Mendieta Castillo <leonardo.mendieta.programer@gmail.com>
2022-11-22 22:32:32 -0400

    Se remplazo tslint por eslint

--- typescript-initializer/src/app.ts
+++ typescript-initializer/src/app.ts
@@ -1,10 +1,10 @@
-import express from 'express';
+import express from 'express'
 
-const app = express();
-const port = 3000;
-app.get('/', (req: any,res: any) => {
-  res.send('Hello world');
+const app = express()
+const port = 3000
+app.get('/', (req: any, res: any) => {
+  res.send('Hello world')
 })
-app.listen(port,() => {
-  console.log(`server is listening on ${port}`);
+app.listen(port, () => {
+  console.log(`server is listening on ${port}`)
 })
