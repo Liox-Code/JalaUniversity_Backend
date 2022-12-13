340fce5b01435ee64e2c23b207915376325b9b0c
Liox <Leonardo.mendieta.programer@gmail.com>
2022-12-13 10:43:46 -0400

    Se agrego express

--- final-project/src/application/controller/snake.controller.ts
+++ final-project/src/application/controller/snake.controller.ts
@@ -1,11 +1,19 @@
 import { Request, Response, NextFunction } from 'express'
 import { controller, httpGet, BaseHttpController } from 'inversify-express-utils'
-
+import { TYPES } from '../../type.core'
+import { SnakeService } from '../../services/snake.service'
+import { inject } from 'inversify'
 @controller('/')
 class IndexHandler extends BaseHttpController {
+  constructor (
+    @inject(TYPES.SnakeService) private userService: SnakeService
+  ) {
+    super()
+  }
+
     @httpGet('/')
   public index (req: Request, res: Response, next: NextFunction) {
-    res.status(200).json({ msg: 'Welcome to Node project' })
+    res.status(200).json({ msg: this.userService.grow() })
   }
 }
 
--- final-project/src/index.ts
+++ final-project/src/index.ts
@@ -1,13 +1,12 @@
-import { container } from './inversify.config'
-import { TYPES } from './type.core'
-import { SnakeService } from './services/snake.service'
-import 'reflect-metadata'
+import App from './app';
 
-class Test {
-  async initializeDb () {
-    const PhotoObj = await container.get<SnakeService>(TYPES.SnakeTypeOrm)
-    PhotoObj.grow()
+(async () => {
+  try {
+    const app = new App()
+    app.initConfig()
+    app.build()
+    app.listen()
+  } catch (err) {
+    console.log(err)
   }
-}
-
-new Test().initializeDb()
+})()
